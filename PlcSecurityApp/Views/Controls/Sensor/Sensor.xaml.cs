79422bade784dd0e52024b34c88a04d3604f93d1
Alyasko <yasko.alexander@gmail.com>
2017-10-10 17:36:42 +0300

    Added Sensor UserControl. Updated UI.

---
+++ PlcSecurityApp/Views/Controls/Sensor/Sensor.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PlcSecurityApp.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for Sensor.xaml
+    /// </summary>
+    public partial class Sensor : UserControl
+    {
+        public ICommand StateChangedCommand
+        {
+            get { return (ICommand)GetValue(StateChangedCommandProperty); }
+            set { SetValue(StateChangedCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StateChangedCommandProperty =
+            DependencyProperty.Register("StateChangedCommand", typeof(ICommand), typeof(Sensor), new PropertyMetadata(null));
+
+
+
+        public string SensorName
+        {
+            get { return (string)GetValue(SensorNameProperty); }
+            set { SetValue(SensorNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SensorName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SensorNameProperty =
+            DependencyProperty.Register("SensorName", typeof(string), typeof(Sensor), new PropertyMetadata("Sensor"));
+
+        public Sensor()
+        {
+            InitializeComponent();
+        }
+
+        private void Sensor_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            StateChangedCommand?.Execute(e);
+        }
+    }
+}
